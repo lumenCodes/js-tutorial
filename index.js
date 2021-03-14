30ea2af764de4d6a1854a36ef9cf4fefaf813b87
cld <lumenluwiz@gmail.com>
2021-03-14 06:31:11 +0100

    Started the introductory part of js

---
+++ index.js
@@ -0,0 +1,25 @@
+// variables
+console.log('Hello Paraclete');
+let name = 'Chinwe';
+console.log('name');
+
+const age = 50
+
+console.log('age');
+
+// objects
+let person = {
+    name: 'Livi',
+    age: 29
+};
+console.log(person);
+
+person.name = 'Ezeh Livinus'
+console.log(person.name)
+
+//arrays
+
+let qualities = ['fair', 'programmer'];
+console.log(qualities)
+qualities[6] = 'kind'; //if you write it without the array literal it will change it to a string
+console.log(qualities)
