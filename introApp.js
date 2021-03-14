9c7b033381c774b03c2226e2ebafc7be6f005ef7
cld <lumenluwiz@gmail.com>
2021-03-14 11:32:14 +0100

    I tried writing an app that introduces a person with hardcoded variables

---
+++ introApp.js
@@ -0,0 +1,12 @@
+let person = {
+    familyname: 'Ezeh',
+    firstName: 'Livinus',
+    residence: "No 4 University road, Nsukka"
+    //let qualities = ['Fair', 'Hairy', 'Intelligent', 'Logical']
+}
+console.log(person);
+
+
+console.log("I have a stubborn friend whose surname is ${person.familyname}")
+console.log(person.familyname)
+console.log('he has {person.length} qualities,' )
