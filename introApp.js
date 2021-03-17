e8fe91e23fa974ad09e8dc7387c39f9dc8264915
cld <lumenluwiz@gmail.com>
2021-03-17 14:07:34 +0100

    fixed some bugs

--- introApp.js
+++ introApp.js
@@ -1,14 +1,14 @@
 let person = {
     familyname: 'Ezeh',
     firstName: 'Livinus',
-    residence: "No 4 University road, Nsukka"
-    //let qualities = ['Fair', 'Hairy', 'Intelligent', 'Logical']
+    residence: "No 4 University road, Nsukka",
+    qualities: ['Fair', 'Hairy', 'Intelligent', 'Logical']
 }
 console.log(person);
 
 
-console.log("I have a stubborn friend whose surname is" + {person.familyname})
+console.log('I have a stubborn friend whose surname is' , person.familyname)
 
-// console.log(person.familyname)
-// console.log('he has {person.length} qualities,')
-// console.log('which are {person.list},')
+console.log(person.familyname)
+console.log(`he has ${person.qualities.length} qualities`)
+console.log('which are ' + person.qualities)
