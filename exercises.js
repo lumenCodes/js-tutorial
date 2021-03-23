201204e87b4738e3b58a4bb8c332c6972f76a4d0
cld <lumenluwiz@gmail.com>
2021-03-23 16:49:28 +0100

    Wrote more objects

---
+++ exercises.js
@@ -0,0 +1,62 @@
+// Addess object
+
+let Address = {
+    street : 'Ekwulobia street',
+    city : 'Awka',
+    zipCode: 001897
+
+};
+function showAddress(Address) {
+    for (let key in Address)
+        console.log(key, Address[key]);
+    }
+
+showAddress(Address);
+
+// constructor function
+function Address1 (street, city, zipcode) {
+    this.street = street;
+    this.city = city;
+    this.zipcode = zipcode;
+}
+
+
+//factory function
+function address2 (street, city, zipcode){
+    return{
+      street,
+      city,
+      zipcode
+    };
+}
+
+// blogPost object
+let blogPost = {
+    title : 'dsd',
+    body : 'jsssssssssssssxxxxxxxxxx deiklweddwe',
+    author : 'jsjfnwu',
+    views : 8388,
+    comments : [
+        {author: 'dfgrf',
+     body: 'kefefefe'},
+     {author: 'dfgrf',
+     body: 'kefefefe'}
+    ],
+    isLive : true
+}
+console.log(blogPost)
+
+// post object
+function Post (title, body, author, isLive){
+    this.title = title;
+    this.body = body;
+    this.author = author;
+    this.views = 0;
+    this.comments = [];
+    this.isLive = false;
+
+}
+console.log(post)
+
+// 
+
